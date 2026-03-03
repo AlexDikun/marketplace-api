1e663f56a73f0dc3e0513e4f943bb395ddc1b4ca
AlexDikun <loban.sasha.97@mail.ru>
2026-03-04 00:29:48 +0300

    добавил в сервис методы, иммитирующие CRUD-операции: на деле они ничего не сохраняют, просто переписывают информацию из одного дто в другое

--- src/main/java/io/github/alexdikun/marketplace/service/AdvertService.java
+++ src/main/java/io/github/alexdikun/marketplace/service/AdvertService.java
@@ -2,7 +2,58 @@ package io.github.alexdikun.marketplace.service;
 
 import org.springframework.stereotype.Service;
 
+import io.github.alexdikun.marketplace.repository.AdvertRepository;
+import io.github.alexdikun.marketplace.request.AdvertRequest;
+import io.github.alexdikun.marketplace.response.AdvertResponse;
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class AdvertService {
-    
+
+    private final AdvertRepository advertRepository;
+
+    public AdvertResponse createAdvert(AdvertRequest advertRequest) {
+        System.out.println("Cоздаем объявление!");
+
+        return AdvertResponse.builder()
+            .id(14L)
+            .name("Mark Tven")
+            .cost(22.5)
+            .address("Sevastopol, 135")
+            .phone("88005553535")
+            .description("nice book")
+            .build();
+    }
+
+    public AdvertResponse getAdvertisementById(Long id) {
+        System.out.println("Получаем объявление по id: " + id);
+
+        return AdvertResponse.builder()
+            .id(id)
+            .name("Mark Tven")
+            .cost(22.5)
+            .address("Sevastopol, 135")
+            .phone("88005553535")
+            .description("nice book")
+            .build();
+    }
+
+    public AdvertResponse updateAdvertisementById(Long id, AdvertRequest advertRequest) {
+        System.out.println("Изменение объявления с id: " + id);
+
+        return AdvertResponse.builder()
+            .id(id)
+            .name(advertRequest.getName())
+            .cost(advertRequest.getCost())
+            .address(advertRequest.getAddress())
+            .phone(advertRequest.getPhone())
+            .description(advertRequest.getDescription())
+            .build();
+    }
+
+    public void deleteAdvertById(Long id) {
+        System.out.println("Удаляем объявление с id: " + id);
+    }
+
 }
