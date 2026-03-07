1669cd7d6418cc931889af2647508c279c3abecd
AlexDikun <loban.sasha.97@mail.ru>
2026-03-07 23:01:02 +0300

    добавил метод, возвращающий все комментарии под объявлением

--- src/main/java/io/github/alexdikun/marketplace/service/CommentService.java
+++ src/main/java/io/github/alexdikun/marketplace/service/CommentService.java
@@ -1,9 +1,11 @@
 package io.github.alexdikun.marketplace.service;
 
+import java.util.List;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 import io.github.alexdikun.marketplace.request.CommentRequest;
+import io.github.alexdikun.marketplace.response.CategoryResponse;
 import io.github.alexdikun.marketplace.response.CommentResponse;
 
 @Service
@@ -31,6 +33,28 @@ public class CommentService {
             .userId(UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE)
             .advertId(UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE)
             .build();
+    }
+
+    public List<CommentResponse> getAllComments(Long advertId) {
+        System.out.println("Получаем список всех комментариев в объявлении!");
+
+        return List.of(
+            CommentResponse.builder()
+            .id(1L)
+            .content("Текст1")
+            .parentId(null)
+            .userId(UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE)
+            .advertId(advertId)
+            .build(),
+
+            CommentResponse.builder()
+            .id(2L)
+            .content("Текст2")
+            .parentId(null)
+            .userId(UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE)
+            .advertId(advertId)
+            .build()
+        );
     }
 
     public CommentResponse updateCommentById(Long id, CommentRequest commentRequest) {
