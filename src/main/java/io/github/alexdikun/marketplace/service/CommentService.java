3b55061bbcc88105d1ccab7cf9461a82a180075c
AlexDikun <loban.sasha.97@mail.ru>
2026-03-08 00:58:57 +0300

    убрал ненужный импорт

--- src/main/java/io/github/alexdikun/marketplace/service/CommentService.java
+++ src/main/java/io/github/alexdikun/marketplace/service/CommentService.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import org.springframework.stereotype.Service;
 
 import io.github.alexdikun.marketplace.request.CommentRequest;
-import io.github.alexdikun.marketplace.response.CategoryResponse;
 import io.github.alexdikun.marketplace.response.CommentResponse;
 
 @Service
