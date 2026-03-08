08874c11243946f23ec8a6418d8398e23cedf1a2
AlexDikun <loban.sasha.97@mail.ru>
2026-03-08 17:15:40 +0300

    сервис с круд операциями для изображений

---
+++ src/main/java/io/github/alexdikun/marketplace/service/ImageService.java
@@ -0,0 +1,44 @@
+package io.github.alexdikun.marketplace.service;
+
+import java.util.UUID;
+
+import io.github.alexdikun.marketplace.request.ImageRequest;
+import io.github.alexdikun.marketplace.response.ImageResponse;
+
+public class ImageService {
+
+    public ImageResponse createImage(Long advertId, ImageRequest imageRequest) {
+        System.out.println("Добавляем изображение к объявлению!");
+
+        return ImageResponse.builder()
+            .id(UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE)
+            .content(imageRequest.getContent())
+            .advertId(imageRequest.getAdvertId())
+            .build();
+    }
+
+    public ImageResponse getImage(Long id) {
+        System.out.println("Получаем изображение по id: " + id);
+
+        return ImageResponse.builder()
+            .id(id)
+            .content("матрица значений, преобразующиеся в пиксели")
+            .advertId(UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE)
+            .build();
+    }
+
+    public ImageResponse getImage(Long id, ImageRequest imageRequest) {
+        System.out.println("Заменяем изображение в объявлении по id: " + id);
+
+        return ImageResponse.builder()
+            .id(id)
+            .content(imageRequest.getContent())
+            .advertId(imageRequest.getAdvertId())
+            .build();
+    }
+
+    public String deleteImageById(Long id) {
+        System.out.println("В объявлении, удаляем изображение с id: " + id);
+        return "Изображение с id: " + id + " удалено!";
+    }
+}
