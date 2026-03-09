2d8164558973bb6e30c7c047bbfeb71def156803
AlexDikun <loban.sasha.97@mail.ru>
2026-03-09 22:21:52 +0300

    в прошылй раз забыл добавить ограничение NOT NULL для User.password

--- src/main/resources/db/migration/V1__init_schema.sql
+++ src/main/resources/db/migration/V1__init_schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE users
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     login VARCHAR(16) NOT NULL UNIQUE,
-    password VARCHAR(255),
+    password VARCHAR(255) NOT NULL,
     role_id INT NOT NULL REFERENCES roles(id),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
