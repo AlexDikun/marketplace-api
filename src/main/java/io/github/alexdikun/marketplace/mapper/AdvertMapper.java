37638d12e32a52b0ec8eff56819392d057d01998
AlexDikun <loban.sasha.97@mail.ru>
2026-03-11 23:23:15 +0300

    написал класс для ресурса Advert для преобразования Advert-dto в AdvertEntity и обратно

---
+++ src/main/java/io/github/alexdikun/marketplace/mapper/AdvertMapper.java
@@ -0,0 +1,29 @@
+package io.github.alexdikun.marketplace.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import io.github.alexdikun.marketplace.entities.AdvertEntity;
+import io.github.alexdikun.marketplace.request.AdvertRequest;
+import io.github.alexdikun.marketplace.response.AdvertResponse;
+    
+@Mapper(componentModel = "spring")
+public interface AdvertMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "category", ignore = true)
+    @Mapping(target = "images", ignore = true)
+    @Mapping(target = "comments", ignore = true)
+    AdvertEntity toAdvertEntity(AdvertRequest advertRequest);
+
+    @Mapping(target = "userId", source = "user.id")
+    @Mapping(target = "categoryId", source = "category.id")
+    @Mapping(target = "images", source = "images")
+    AdvertResponse toAdvertResponse(AdvertEntity advertEntity);
+
+    List<AdvertResponse> toListAdvertResponse(List<AdvertEntity> advertEntities);
+
+}
