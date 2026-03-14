72b2dadda61b52217d13cda5c7835ceb7c870f31
AlexDikun <loban.sasha.97@mail.ru>
2026-03-15 00:21:49 +0300

    по аналогии с остальными мапперами, доабвил метод для обновления сущности

--- src/main/java/io/github/alexdikun/marketplace/mapper/CommentMapper.java
+++ src/main/java/io/github/alexdikun/marketplace/mapper/CommentMapper.java
@@ -2,8 +2,11 @@ package io.github.alexdikun.marketplace.mapper;
 
 import java.util.List;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import io.github.alexdikun.marketplace.entities.CommentEntity;
 import io.github.alexdikun.marketplace.request.CommentRequest;
@@ -22,6 +25,12 @@ public interface CommentMapper {
     @Mapping(target = "userId", source = "user.id")
     @Mapping(target = "advertId", source = "advert.id")
     CommentResponse toCommentResponse(CommentEntity commentEntity);
+
+    @Mapping(target = "parentComment", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "advert", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateCommentFromDto(CommentRequest commentRequest, @MappingTarget CommentEntity commentEntity);
 
     List<CommentResponse> toCommentResponseList(List<CommentEntity> entities);
 }
