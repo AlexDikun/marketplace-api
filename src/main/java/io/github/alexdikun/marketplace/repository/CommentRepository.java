04796afe9232a4031039f691174bafa7e67d3f46
AlexDikun <loban.sasha.97@mail.ru>
2026-03-15 01:18:16 +0300

    добавил метод поиска всех комментариев по id объявления

--- src/main/java/io/github/alexdikun/marketplace/repository/CommentRepository.java
+++ src/main/java/io/github/alexdikun/marketplace/repository/CommentRepository.java
@@ -1,9 +1,13 @@
 package io.github.alexdikun.marketplace.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import io.github.alexdikun.marketplace.entities.CommentEntity;
 
 @Repository
-public interface CommentRepository extends JpaRepository<CommentEntity, Long> {}
+public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+    List<CommentEntity> findByAdvertId(Long advertId);
+}
