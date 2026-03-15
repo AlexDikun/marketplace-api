0c9766f6c083917bf10d95c03a00181c8da2e333
AlexDikun <loban.sasha.97@mail.ru>
2026-03-15 22:21:10 +0300

    класс для получения пути к локальному хранилищу

---
+++ src/main/java/io/github/alexdikun/marketplace/config/StorageProperties.java
@@ -0,0 +1,15 @@
+package io.github.alexdikun.marketplace.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Configuration
+@ConfigurationProperties(prefix = "storage")
+@Data
+public class StorageProperties {
+
+    private String uploadDir;
+    
+}
