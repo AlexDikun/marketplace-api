2aba77ff954d004cd92cab857c26870336be820a
AlexDikun <loban.sasha.97@mail.ru>
2026-03-16 00:14:33 +0300

    удалил ImageRequest + в маппере удалил метод по конвертированию из request в entity

--- src/main/java/io/github/alexdikun/marketplace/mapper/ImageMapper.java
+++ src/main/java/io/github/alexdikun/marketplace/mapper/ImageMapper.java
@@ -1,18 +1,17 @@
 package io.github.alexdikun.marketplace.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import io.github.alexdikun.marketplace.entities.ImageEntity;
-import io.github.alexdikun.marketplace.request.ImageRequest;
 import io.github.alexdikun.marketplace.response.ImageResponse;
 
 @Mapper(componentModel = "spring")
 public interface ImageMapper {
-    
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "advert", ignore = true)
-    ImageEntity toImageEntity(ImageRequest imageRequest);
 
     ImageResponse toImageResponse(ImageEntity imageEntity);
+
+    List<ImageResponse> toImageResponseList(List<ImageEntity> imageEntities);
+    
 }
--- src/main/java/io/github/alexdikun/marketplace/request/ImageRequest.java
+++
@@ -1,9 +0,0 @@
-package io.github.alexdikun.marketplace.request;
-
-import lombok.Data;
-
-@Data
-public class ImageRequest {
-    private String url;
-    private Long advertId;
-}
