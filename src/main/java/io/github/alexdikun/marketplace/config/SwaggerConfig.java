2ed14a9374b727695492255e62e5ef927a62157c
AlexDikun <loban.sasha.97@mail.ru>
2026-03-28 21:20:47 +0300

    добавил кнопку Авторизация в сваггер

--- src/main/java/io/github/alexdikun/marketplace/config/SwaggerConfig.java
+++ src/main/java/io/github/alexdikun/marketplace/config/SwaggerConfig.java
@@ -3,8 +3,10 @@ package io.github.alexdikun.marketplace.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
@@ -12,9 +14,18 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI api() {
         return new OpenAPI()
-                .info(new Info().title("Marketplace service")
-                        .version("1.0")
-                        .description("Marketplace service API"));
+            .info(new Info().title("Marketplace service")
+                .version("1.0")
+                .description("Marketplace service API"))
+            .components(new Components()
+                .addSecuritySchemes("bearerAuth", 
+                    new SecurityScheme()
+                        .name("bearerAuth")
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+            )
+        );
     }
 
 }
