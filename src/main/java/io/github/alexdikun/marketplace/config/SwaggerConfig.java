bd817111575e998f59dec7686c4244451d8cf40f
AlexDikun <loban.sasha.97@mail.ru>
2026-03-29 21:24:21 +0300

    есть инфа, что без ecurityRequirement сваггер не отправляет токен

--- src/main/java/io/github/alexdikun/marketplace/config/SwaggerConfig.java
+++ src/main/java/io/github/alexdikun/marketplace/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
@@ -25,7 +26,8 @@ public class SwaggerConfig {
                         .scheme("bearer")
                         .bearerFormat("JWT")
             )
-        );
+        )
+        .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 
 }
