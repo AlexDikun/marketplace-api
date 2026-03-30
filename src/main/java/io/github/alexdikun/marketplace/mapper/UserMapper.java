74b85d81fe9f06e1389f13b441b58f89c0545994
AlexDikun <loban.sasha.97@mail.ru>
2026-03-31 01:48:59 +0300

    в штангу дал - в UserResponse и не было ж тех полей

--- src/main/java/io/github/alexdikun/marketplace/mapper/UserMapper.java
+++ src/main/java/io/github/alexdikun/marketplace/mapper/UserMapper.java
@@ -20,9 +20,6 @@ public interface UserMapper {
     @Mapping(target = "login", ignore = true)
     UserEntity toUserEntity(UserRequest userRequest);
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "keycloakId", ignore = true)
-    @Mapping(target = "login", ignore = true)
     UserResponse toUserResponse(UserEntity userEntity);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
