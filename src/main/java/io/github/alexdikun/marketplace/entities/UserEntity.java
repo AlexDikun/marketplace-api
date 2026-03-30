6bf0f002cf676c1a3e6e6140ceef80f76ac3fc8d
AlexDikun <loban.sasha.97@mail.ru>
2026-03-30 23:07:27 +0300

    добавил поля с имейлом и айди_кейлоком

--- src/main/java/io/github/alexdikun/marketplace/entities/UserEntity.java
+++ src/main/java/io/github/alexdikun/marketplace/entities/UserEntity.java
@@ -6,15 +6,11 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -27,18 +23,17 @@ public class UserEntity {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "name", nullable = false, length = 50)
-    @NotBlank(message = "Имя пользователя не должно быть пустым")
-    private String name;
+    @Column(name = "keycloak_id", unique = true, nullable = false)
+    @NotBlank(message = "Кейлок-идентификатор не должно быть пустым")
+    private String keycloakId;
+
+    @Column(name = "email", nullable = false, length = 50)
+    @NotBlank(message = "Имейл не должно быть пустым")
+    private String email;
 
     @Column(name = "login", unique = true, nullable = false, length = 16)
     @NotBlank(message = "Логин не должен быть пустым")
     private String login;
-
-    @Column(name = "password", nullable = false)
-    @Size(min = 8, message = "Слишком короткий пароль")
-    @NotBlank(message = "Пароль не должен быть пустым")
-    private String password;
 
     @Column(name = "created_at", nullable = false, updatable = false)
     @CreationTimestamp
