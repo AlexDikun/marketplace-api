15e5aab5b0bbd8b94ef4a88ca01a995485bbf0ff
AlexDikun <loban.sasha.97@mail.ru>
2026-03-30 22:43:53 +0300

    удалил сущность, енамс и репозиторий для ролей

--- src/main/java/io/github/alexdikun/marketplace/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package io.github.alexdikun.marketplace.enums;
-
-public enum Role { 
-    ROLE_USER, 
-    ROLE_ADMIN
-}
--- src/main/java/io/github/alexdikun/marketplace/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.alexdikun.marketplace.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import io.github.alexdikun.marketplace.entities.RoleEntity;
-
-@Repository
-public interface RoleRepository extends JpaRepository<RoleEntity, Long> {}
