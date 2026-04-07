27d039d6c489bb61bf560fc05d222daa295e3e84
AlexDikun <loban.sasha.97@mail.ru>
2026-04-08 00:10:38 +0300

    логи для юзер сервиса

--- src/main/java/io/github/alexdikun/marketplace/service/UserService.java
+++ src/main/java/io/github/alexdikun/marketplace/service/UserService.java
@@ -3,16 +3,16 @@ package io.github.alexdikun.marketplace.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import io.github.alexdikun.marketplace.entities.UserEntity;
 import io.github.alexdikun.marketplace.exceptions.NotFoundException;
 import io.github.alexdikun.marketplace.mapper.UserMapper;
 import io.github.alexdikun.marketplace.repository.UserRepository;
-import io.github.alexdikun.marketplace.request.UserRequest;
 import io.github.alexdikun.marketplace.response.UserResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -21,17 +21,20 @@ public class UserService {
     private final UserMapper userMapper;
     
     public List<UserResponse> getAllUsers() {
-        System.out.println("Получаем список всех пользователей!");
+        log.info("Получаем список всех пользователей!");
 
         List<UserEntity> allUsers = userRepository.findAll();
         return userMapper.toListUserResponse(allUsers);
     } 
     
     public UserResponse getUser(Long id) {
-        System.out.println("Получаем пользователя по id: " + id);
+        log.info("Получаем пользователя. userId = {}", id);
 
         UserEntity userEntity = userRepository.findById(id)
-            .orElseThrow(() -> new NotFoundException("Пользователь не найден"));
+            .orElseThrow(() -> {
+                log.warn("Пользователь не найден. userId = {}", id);
+                return new NotFoundException("Пользователь не найден");
+            });
 
         return userMapper.toUserResponse(userEntity);
     }
